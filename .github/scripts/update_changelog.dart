30b13c51ddf58ece84ec9886e977d5b033be94ed
robmllze <86869208+robmllze@users.noreply.github.com>
2025-06-11 15:52:11 -0400

    update

--- .github/scripts/update_changelog.dart
+++ .github/scripts/update_changelog.dart
@@ -40,9 +40,10 @@ void main(List<String> args) {
       ),
     );
   }
-  contents = '# Changelog\n\n${(sections.toList()..sort((a, b) {
-      return compareVersions(b.version, a.version);
-    })).map((e) => e.toString()).join('\n')}';
+  contents =
+      '# Changelog\n\n${(sections.toList()..sort((a, b) {
+        return compareVersions(b.version, a.version);
+      })).map((e) => e.toString()).join('\n')}';
 
   file.writeAsStringSync(contents);
   print('Changelog updated with version $version.');
@@ -59,8 +60,9 @@ Set<_VersionSection> extractSections(String contents) {
     final end = i + 1 < allVersionMatches.length ? allVersionMatches[i + 1].start : contents.length;
     final sectionContents = contents.substring(start, end).trim();
     final lines = sectionContents.split('\n').where((line) => line.isNotEmpty).toList();
-    final version =
-        allVersionMatches[i].group(0)!.substring(4, allVersionMatches[i].group(0)!.length - 1);
+    final version = allVersionMatches[i]
+        .group(0)!
+        .substring(4, allVersionMatches[i].group(0)!.length - 1);
     var releasedAt = DateTime.now().toUtc();
     final updates = <String>{};
     final old = lines
@@ -76,13 +78,7 @@ Set<_VersionSection> extractSections(String contents) {
         updates.add(line);
       }
     }
-    results.add(
-      _VersionSection(
-        version: version,
-        releasedAt: releasedAt,
-        updates: updates,
-      ),
-    );
+    results.add(_VersionSection(version: version, releasedAt: releasedAt, updates: updates));
   }
 
   return results;
@@ -103,11 +99,8 @@ class _VersionSection {
   //
   //
 
-  _VersionSection({
-    required this.version,
-    required this.releasedAt,
-    this.updates = const {},
-  });
+  _VersionSection({required this.version, required this.releasedAt, Set<String>? updates})
+    : this.updates = updates ?? {};
 
   //
   //
